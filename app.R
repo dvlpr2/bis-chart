cd455fc7649db8871f4d3e49f7efc294c960edb1
dvlpr2 <dvlpr-github@protonmail.com>
2024-09-01 09:53:17 +0200

    Remove links

--- app.R
+++ app.R
@@ -1,1 +1,31 @@
-../2.bis-chart-orig/app.R
+library(shiny)
+source("./bis-chart.R")
+
+ui <- fluidPage(
+  hr(style = "border-top: 10px solid #000000;"),
+  titlePanel("OTC derivatives statistics produced with R"),
+  hr(style = "border-top: 10px solid #000000;"),
+  plotOutput("plot", width="650px", height="650px"),
+  hr(),
+  hr(style = "border-top: 10px solid #000000;"),
+  titlePanel(tags$h1("OTC derivatives statistics at end-June 2023 [Original chart from BIS]")),
+  titlePanel(tags$h4(
+    tags$a("BIS original",href="https://www.bis.org/publ/otc_hy2311.htm")
+  )),
+  hr(style = "border-top: 10px solid #000000;"),
+  tags$figure(
+    class = "centerFigure",
+    tags$img(
+      src = "https://www.bis.org/publ/otc_hy2311/images/ch1graph1.jpg",
+      width = 1400,
+    )
+  )
+)
+
+server <- function(input, output, session) {
+  output$plot <- renderPlot({
+    draw_bis_chart()
+  }, res = 50)
+}
+
+shinyApp(ui, server)
